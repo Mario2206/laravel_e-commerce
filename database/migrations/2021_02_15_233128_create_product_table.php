87943f6505f41af554d171a809c3ce7f983ad9c2
mathieu raimbault <mathieu.raimbault@hotmail.com>
2021-02-16 01:11:13 +0100

    :tada: init project structure

---
+++ database/migrations/2021_02_15_233128_create_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->float('price');
+            $table->longText('img_path');
+            $table->longText('description');
+            $table->integer('stock');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
